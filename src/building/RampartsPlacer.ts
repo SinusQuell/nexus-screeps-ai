7aeb76252598f4dbfe3d69365c90661327976043
SinusQuell <65397804+SinusQuell@users.noreply.github.com>
2023-01-22 00:11:55 +0100

    cleanup

--- src/building/RampartsPlacer.ts
+++ src/building/RampartsPlacer.ts
@@ -4,27 +4,19 @@ export class RampartsPlacer {
     public safeRects: SafeRect[]
     public roomName: string
 
-    public wallPositions?: {x: number, y: number}[];
+    public wallPositions?: { x: number, y: number }[]
 
-    private bounds = { x1: 0, y1: 0, x2: 49, y2: 49 };
+    private bounds = { x1: 0, y1: 0, x2: 49, y2: 49 }
 
     constructor(roomName: string, safeRects: SafeRect[]) {
-        this.safeRects = safeRects;
-        this.roomName = roomName;
-      }
+        this.safeRects = safeRects
+        this.roomName = roomName
+    }
 
-    public test() {
-		this.wallPositions = util_mincut.GetCutTiles(this.roomName, this.safeRects, this.bounds); // Positions is an array where to build walls/ramparts
-        this.visualize()
-		// const ramparts = Game.rooms[roomname].find(FIND_STRUCTURES, {filter: s => s.structureType === 'rampart'});
-		// const rampartsC = Game.rooms[roomname].find(FIND_CONSTRUCTION_SITES, {filter: c => c.structureType === 'rampart'});
-		// if (rampartsC.length + ramparts.length < positions.length) {
-		// 	for (let i in positions) {
-		// 		Game.rooms[roomname].createConstructionSite(positions[i].x, positions[i].y, 'rampart');
-		// 	}
-		// }
-		// return 'Finished';
-	}
+    public calculate(visualize: boolean = true) {
+        this.wallPositions = util_mincut.GetCutTiles(this.roomName, this.safeRects, this.bounds) // Positions is an array where to build walls/ramparts
+        if (visualize) this.visualize()
+    }
 
     private visualize() {
         // draw rectangles for the designated safe areas
@@ -40,28 +32,28 @@ export class RampartsPlacer {
         })
     }
 }
-export class SafeRect {
-    public x1: number;
-    public y1: number;
-    public x2: number;
-    public y2: number;
 
-    public minX: number;
-    public minY: number;
-    public width: number;
-    public height: number;
+export class SafeRect {
+    public x1: number
+    public y1: number
+    public x2: number
+    public y2: number
+
+    public minX: number
+    public minY: number
+    public width: number
+    public height: number
 
     constructor(x1: number, y1: number, x2: number, y2: number) {
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
+        this.x1 = x1
+        this.y1 = y1
+        this.x2 = x2
+        this.y2 = y2
 
-        this.minX   = Math.min(this.x1, this.x2);
-        this.minY   = Math.min(this.y1, this.y2);
-        this.width  = Math.max(this.x1, this.x2) - Math.min(this.x1, this.x2);
+        this.minX = Math.min(this.x1, this.x2)
+        this.minY = Math.min(this.y1, this.y2)
+        this.width = Math.max(this.x1, this.x2) - Math.min(this.x1, this.x2)
         this.height = Math.max(this.y1, this.y2) - Math.min(this.y1, this.y2)
     }
-
 
 }
--- src/main.ts
+++ src/main.ts
@@ -14,24 +14,24 @@ declare global {
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
-	console.log(`Current game tick is ${Game.time}`);
+	console.log(`Current game tick is ${Game.time}`)
 
-	let cpu = Game.cpu.getUsed();
+	let cpu = Game.cpu.getUsed()
 
-	let rect_array: SafeRect[] = [];
-	rect_array.push(new SafeRect(20, 6, 28, 27));
-	rect_array.push(new SafeRect(29, 13, 34, 16));
+	let rect_array: SafeRect[] = []
+	rect_array.push(new SafeRect(20, 6, 28, 27))
+	rect_array.push(new SafeRect(29, 13, 34, 16))
 
-	let ramps = new RampartsPlacer('sim', rect_array);
-	ramps.test();
+	let ramps = new RampartsPlacer('sim', rect_array)
+	ramps.calculate()
 
-	cpu = Game.cpu.getUsed() - cpu;
-	console.log('Needed', cpu, ' cpu time');
+	cpu = Game.cpu.getUsed() - cpu
+	console.log('Needed', cpu, ' cpu time')
 
 	// Automatically delete memory of missing creeps
 	for (const name in Memory.creeps) {
 		if (!(name in Game.creeps)) {
-			delete Memory.creeps[name];
+			delete Memory.creeps[name]
 		}
 	}
 });
