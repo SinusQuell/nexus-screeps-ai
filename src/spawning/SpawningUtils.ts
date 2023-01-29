a7ccff0777090d517d8eaa8447ff2557ef1d60b9
SinusQuell <65397804+SinusQuell@users.noreply.github.com>
2023-01-29 18:58:12 +0100

    added cost calculation for creeps

--- src/spawning/SpawningUtils.ts
+++ src/spawning/SpawningUtils.ts
@@ -1,20 +1,24 @@
-// Expample call: bodyBuilder().work(2).carry(1).move(1).finalise();
+// Expample 1: bodyBuilder().work(2).carry(1).move(1).finalise();
 // returns: [WORK, WORK, CARRY, MOVE]
-const bodyImpl = (parts: BodyPartConstant[]) => {
-  const add = (part: BodyPartConstant, count: number) =>
-    bodyImpl(parts.concat(Array(count).map(() => part)));
+// Expample 2: bodyBuilder().work(2).carry(1).move(1).cost();
+// returns: 300
+const bodyImpl = (parts: BodyPartConstant[], cost: number) => {
+	const add = (part: BodyPartConstant, count: number, cost: number) => {
+		return bodyImpl(parts.concat(Array(count).map(() => part)), cost * count);
+	}
 
-  return {
-    work: (count: number) => add(WORK, count),
-    carry: (count: number) => add(CARRY, count),
-    move: (count: number) => add(MOVE, count),
-    tough: (count: number) => add(TOUGH, count),
-    attack: (count: number) => add(ATTACK, count),
-    rangedAttack: (count: number) => add(RANGED_ATTACK, count),
-    heal: (count: number) => add(HEAL, count),
-    claim: (count: number) => add(CLAIM, count),
-    finalise: () => parts,
-  };
+	return {
+		work: (count: number) => add(WORK, count, 100),
+		carry: (count: number) => add(CARRY, count, 50),
+		move: (count: number) => add(MOVE, count, 50),
+		tough: (count: number) => add(TOUGH, count, 10),
+		attack: (count: number) => add(ATTACK, count, 80),
+		rangedAttack: (count: number) => add(RANGED_ATTACK, count, 150),
+		heal: (count: number) => add(HEAL, count, 250),
+		claim: (count: number) => add(CLAIM, count, 600),
+		finalise: () => parts,
+		cost: () => cost,
+	};
 };
 
-export const bodyBuilder = () => bodyImpl([]);
+export const bodyBuilder = () => bodyImpl([], 0);
