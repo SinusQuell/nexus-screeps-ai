b9b81bf7831055d51f6a55f2efa00b7024af3977
SinusQuell <65397804+SinusQuell@users.noreply.github.com>
2023-01-31 23:23:03 +0100

    finished miner spawning

--- src/spawning/Progenitor.ts
+++ src/spawning/Progenitor.ts
@@ -1,6 +1,6 @@
 import { TaskMineMemory, TaskType } from "../creeps/tasks/Task"
 import { TaskMine } from "../creeps/tasks/TaskMine"
-import { createBody, getCostByParts } from "./SpawningUtils"
+import { createBody, getCostByParts, getCostByPartsArray } from "./SpawningUtils"
 
 export class Progenitor {
 
@@ -13,25 +13,41 @@ export class Progenitor {
         if (!mineTasks.length) return;
 
         mineTasks.forEach(t => {
-            let creepCost = getCostByParts(t.requiredParts)
+            let creepCostFull = getCostByParts(t.requiredParts)
 
             // if creepCost is smaller than room.energyCapacityAvailable (so we can spawn the whole creep),
             // then creepAmount will be 1. Otherwise, it will be larger.
-            let creepAmount = Math.ceil(creepCost / room.energyCapacityAvailable)
+            let creepAmount = Math.ceil(creepCostFull / room.energyCapacityAvailable)
 
             // look for creeps already doing this task
-            let creeps = _.filter(Game.creeps, c => c.memory.task.id == t.id)
+            let creeps = _.filter(Game.creeps, c => c.memory.task?.id == t.id && c.memory.homeRoomName == room.name)
             if (creeps.length >= creepAmount) return
 
-            // spawn missing creeps
+            // create fitting body
             let body = createBody(room.energyCapacityAvailable, t.requiredParts)
-            for (let i = 0; i < creepAmount-creeps.length; i++) {
-                // find free spawn in the room
-                // spawn missing creeps
-            }
+            let creepCostFinal = getCostByPartsArray(body)
 
+            // TODO: adjust taskPosition when splitting Task to multiple creeps.
+
+            // spawn missing creeps
+            for (let i = 0; i < creepAmount-creeps.length; i++) {
+                let spawn = this.findFreeSpawn(room)
+                if (spawn && room.energyAvailable >= creepCostFinal) {
+                    spawn.spawnCreep(body, `${t.taskType}${new Date().getTime()}`, { memory: { homeRoomName: room.name, task: t,}})
+                }
+            }
         });
     }
 
+    private static findFreeSpawn(room: Room): StructureSpawn | false {
+        let spawns = room.find(FIND_MY_SPAWNS, {
+            filter: (structure) => {
+                return (structure.spawning == null);
+            }
+        });
+        if (spawns.length > 0)
+            return spawns[0];
 
+        return false
+    }
 }
