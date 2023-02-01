53c6476b969d1721b7f798eeb2103565c5654173
SinusQuell <65397804+SinusQuell@users.noreply.github.com>
2023-02-02 00:20:35 +0100

    implemented miner task

--- src/creeps/tasks/TaskMine.ts
+++ src/creeps/tasks/TaskMine.ts
@@ -1,11 +1,20 @@
-import { Task } from "./Task"
+import { Task, TaskMineMemory } from "./Task"
 
 export class TaskMine extends Task {
     run(creep: Creep): ScreepsReturnCode {
-        // move to position
-        // mine
+        // get data
+        let task: TaskMineMemory = creep.memory.task!
+        let source = creep.room.find(FIND_SOURCES)[task.sourceIndex!]
+        if (!task || !source) return ERR_NOT_FOUND
 
+        // move to source
+        if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+            // use saved mining position or just path to source?
+            if (task.useTaskPosition && task.pos)
+                creep.travelTo(task, {ensurePath: true})
+            else creep.travelTo(source.pos)
+            return OK
+        }
         return OK
     }
-
 }
--- src/spawning/Progenitor.ts
+++ src/spawning/Progenitor.ts
@@ -1,6 +1,8 @@
 import { TaskMineMemory, TaskType } from "../creeps/tasks/Task"
 import { TaskMine } from "../creeps/tasks/TaskMine"
 import { createBody, getCostByParts, getCostByPartsArray } from "./SpawningUtils"
+import { SourceHelper } from "../utils/SourceHelper";
+import { clone } from "lodash";
 
 export class Progenitor {
 
@@ -13,6 +15,7 @@ export class Progenitor {
         if (!mineTasks.length) return;
 
         mineTasks.forEach(t => {
+            t = clone(t)
             let creepCostFull = getCostByParts(t.requiredParts)
 
             // if creepCost is smaller than room.energyCapacityAvailable (so we can spawn the whole creep),
@@ -27,10 +30,18 @@ export class Progenitor {
             let body = createBody(room.energyCapacityAvailable, t.requiredParts)
             let creepCostFinal = getCostByPartsArray(body)
 
-            // TODO: adjust taskPosition when splitting Task to multiple creeps.
+            //get source info
+            let source = room.find(FIND_SOURCES)[t.sourceIndex!]
+            let spaces = SourceHelper.getSourceSpaceAmount(source)
+
+            // adjust creep amount if source is low on space
+            if (spaces < creepAmount) creepAmount = spaces
 
             // spawn missing creeps
             for (let i = 0; i < creepAmount-creeps.length; i++) {
+                // adjust task so first creep always goes to the taskPosition, others just path there
+                if (creepAmount > 1 && i == 0) t.useTaskPosition = true
+
                 let spawn = this.findFreeSpawn(room)
                 if (spawn && room.energyAvailable >= creepCostFinal) {
                     spawn.spawnCreep(body, `${t.taskType}${new Date().getTime()}`, { memory: { homeRoomName: room.name, task: t,}})
